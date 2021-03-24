93a8eda8d080dff61a0b84cdc207efaaaaf56166
Joseph <Josephlewis1992@gmail.com>
2021-03-24 15:29:08 +0000

    documentation on abc_reject

--- R/abc_reject.R
+++ R/abc_reject.R
@@ -1,4 +1,24 @@
+#' Rejection sampling scheme for ABC
+#'
+#' This function applies the rejection sampling scheme based on the efficacy of the summary statistic
+#'
+#' @param parameters prior parameter values
+#'
+#' @param lines SpatialLines
+#'
+#' @param summary_stat_target a vector of the simulated summary statistic
+#'
+#' @param tol tolerance, the required proportion of points accepted nearest the target values
+#'
+#' @return list of accepted parameter values
+#'
 #' @keywords internal
+#'
+#' @author Joseph Lewis
+#'
+#' @import foreach
+#' @import sp
+#' @import methods
 
 abc_reject <- function(parameters = processed_params, lines = lines, summary_stat_target = summary_stat_target , tol = tol) {
 
@@ -7,5 +27,3 @@ abc_reject <- function(parameters = processed_params, lines = lines, summary_sta
   return(parameters_reject)
 
 }
-
-

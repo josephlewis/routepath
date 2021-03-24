64257848a6c3bd6116cf1bab6ca7d361f5429b8d
Joseph <Josephlewis1992@gmail.com>
2021-03-24 15:28:58 +0000

    documentation on abc_index

--- R/abc_index.R
+++ R/abc_index.R
@@ -1,4 +1,18 @@
+#' Identify accepted simulations
+#'
+#' This function returns an index of the identified accepted simulations
+#'
+#' @param parameters accepted parameter values
+#'
+#' @return vector index of accepted simulations
+#'
 #' @keywords internal
+#'
+#' @author Joseph Lewis
+#'
+#' @import foreach
+#' @import sp
+#' @import methods
 
 abc_index <- function(parameters = param_reject) {
 

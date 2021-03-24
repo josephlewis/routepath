f57d62d03613fe929d07793d08b1c657a170c7a3
Joseph <Josephlewis1992@gmail.com>
2021-03-24 11:41:53 +0000

    split process_parameters into two functions (abc_index and process_abc)

--- R/process_parameters.R
+++ R/process_parameters.R
@@ -1,6 +1,6 @@
 #' @keywords internal
 
-process_parameters <- function(routepaths, lines, priors, validation, summary_stat_target, tol) {
+process_parameters <- function(routepaths, lines, priors, validation) {
 
   points_matrix <- matrix(c(1:length(routepaths), rep(1:length(lines), times = nrow(priors))), ncol = 2)
 
@@ -15,12 +15,6 @@ process_parameters <- function(routepaths, lines, priors, validation, summary_st
 
   parameters <- split.data.frame(parameters, parameters[,"line_id"])
 
-  parameters_abc <- lapply(X = parameters, FUN = function(x) { rej <- abc::abc(target = summary_stat_target, param = x[,2:ncol(x)], sumstat = x[,"stats"], tol = tol, method = "rejection")
-  })
-
-  parameters_abc <- cbind(line_id = rep(1:length(lines), each = sum(parameters_abc[[1]]$region)),
-                          do.call(rbind, lapply(X = parameters_abc, FUN = function(x) { x$unadj.values})))
-
-  return(parameters_abc)
+  return(parameters)
 
 }

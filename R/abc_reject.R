d302ed41bca2bfc908c40aa98d6ce7dea8e9b06c
Joseph <Josephlewis1992@gmail.com>
2021-03-24 11:40:22 +0000

    calculate index of accepted simulations by ABC

--- R/abc_reject.R
+++ R/abc_reject.R
@@ -1,10 +1,11 @@
-# abc_reject <- function(parameters, lines) {
-#
-#   parameters_reject <- lapply(X = parameters, FUN = function(x) { abc::abc(target = summary_stat_target, param = x[,2:ncol(x)], sumstat = x[,"stats"], tol = tol, method = "rejection")})
-#
-#   parameters_abc <- cbind(line_id = rep(1:length(lines), each = sum(parameters_reject[[1]]$region)),
-#                         do.call(rbind, lapply(X = parameters_reject, FUN = function(x) { x$unadj.values})))
-#
-# }
-#
-#
+#' @keywords internal
+
+abc_reject <- function(parameters = processed_params, lines = lines, summary_stat_target = summary_stat_target , tol = tol) {
+
+  parameters_reject <- lapply(X = parameters, FUN = function(x) { abc::abc(target = summary_stat_target, param = x[,2:ncol(x)], sumstat = x[,"stats"], tol = tol, method = "rejection")})
+
+  return(parameters_reject)
+
+}
+
+

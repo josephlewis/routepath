00d8b7515f37af1a8b8f93cf7131ce3f4e1735d7
Joseph <Josephlewis1992@gmail.com>
2021-04-15 16:11:41 +0100

    fixed tolerance and retain matrix form after subsetting (drop = FALSE)

--- R/abc_reject.R
+++ R/abc_reject.R
@@ -20,9 +20,9 @@
 #' @import sp
 #' @import methods
 
-abc_reject <- function(parameters = processed_params, lines = lines, summary_stat_target = summary_stat_target , tol = tol) {
+abc_reject <- function(parameters = processed_params, lines = lines, summary_stat_target = 0, tol = 1) {
 
-  parameters_reject <- lapply(X = parameters, FUN = function(x) { abc::abc(target = summary_stat_target, param = x[,2:ncol(x)], sumstat = x[,"stats"], tol = tol, method = "rejection")})
+  parameters_reject <- lapply(X = parameters, FUN = function(x) { abc::abc(target = summary_stat_target, param = x[,2:ncol(x)], sumstat = x[,"stats", drop = FALSE], tol = tol, method = "rejection")})
 
   return(parameters_reject)
 

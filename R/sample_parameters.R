052efbee48650c094961d9ec81b14ee2664cdc09
Joseph <Josephlewis1992@gmail.com>
2021-06-13 12:21:19 +0100

    sample_parameters

--- R/sample_parameters.R
+++ R/sample_parameters.R
@@ -1,3 +1,15 @@
+#' Extract parameter values from routepaths
+#'
+#' @param parameters Extract parameter values from routepaths
+#'
+#' @param no_samples routepaths
+#'
+#' @return parameter values
+#'
+#' @author Joseph Lewis
+#'
+#' @export
+
 sample_parameters <- function(parameters, no_samples) {
 
   parameters <- apply(X = parameters, MARGIN = 2, sample, no_samples, replace = TRUE)

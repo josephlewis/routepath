279a361edbb98fc17bd894cba9fa63532b343737
Joseph <Josephlewis1992@gmail.com>
2022-07-03 08:41:25 +0100

    amended argument name

--- R/summarise_parameters.R
+++ R/summarise_parameters.R
@@ -1,10 +1,26 @@
-summarise_parameters <- function(routepaths, col_index, fun) {
+#' Summarise parameters
+#'
+#' @param routes simulated routes
+#'
+#' @param col_index ...
+#'
+#' @param fun ....
+#'
+#' @return df
+#'
+#' @author Joseph Lewis
+#'
+#' @export
 
-  if(any(col_index > ncol(routepaths))){
+summarise_parameters <- function(routes, col_index, fun) {
+
+  # do if col_index NULL then do all. check other functions for how I've done this
+
+  if(any(col_index > ncol(routes))){
     stop("col_index outside of possible columns")
   }
 
-  accepted_routepaths <- routepaths[routepaths$result == "Accept",]
+  accepted_routepaths <- routes[routes$result == "Accept",]
   func_vector <- rep(fun, length.out = length(col_index))
 
   params <- list()
@@ -12,7 +28,7 @@ summarise_parameters <- function(routepaths, col_index, fun) {
   for(i in 1:length(col_index)) {
     summarised_param <- stats::aggregate(accepted_routepaths[[col_index[i]]], list(accepted_routepaths$line_id), func_vector[i])
     summarised_param <- summarised_param[,2, drop = FALSE]
-    colnames(summarised_param) <- colnames(accepted_routepaths)[col_index[i]]
+    colnames(summarised_param) <- paste0(colnames(accepted_routepaths)[col_index[i]], "_", func_vector[i])
     params[[i]] <- summarised_param
   }
 
